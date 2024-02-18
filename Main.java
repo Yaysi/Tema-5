8c4126de3ed4599ba0f9d5c798dccd1c6ad4fb17
Ismail <ismailtikhlal74@gmail.com>
2024-02-18 18:04:18 +0200

    Java Program

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the first Number");
+        int n1 = sc.nextInt();
+        System.out.println("Input the second Number");
+        int n2 = sc.nextInt();
+        System.out.println("Input the third Number");
+        int n3 = sc.nextInt();
+
+        System.out.println(n1 + "+" + n2 +"+"+n3+ "=" +(n1+n2+n3));
+    }
+}
